03d60b8a1ce70ec13d7c2c653904ec35c72ff126
Harihar-HHS <30701256+Harihar-HHS@users.noreply.github.com>
2018-08-30 12:50:16 +0530

    registration changes

--- app/src/main/java/com/example/hhs/loginregister/login.java
+++ app/src/main/java/com/example/hhs/loginregister/login.java
@@ -1,16 +1,35 @@
 package com.example.hhs.loginregister;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
 
     TextView uid,lgin_pswd;
     Button lgin;
     String uid_mobile,login_pswd;
+
+    int isSignedIn=0;
+
+    private ProgressDialog progressDialog;
+    private static final String TAG = login.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +45,97 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
                 uid_mobile=uid.getText().toString();
                 login_pswd=lgin_pswd.getText().toString();
+
+                signin();
             }
         });
+    }
+
+    private void signin() {
+        if(isSignedIn==1){          // goto myevents screen
+
+        }
+        else{
+            /*if (!validate()) {
+            onSignupFailed();
+            return;
+            }*/
+
+            progressDialog.setMessage("Signing in ...");
+            showDialog();
+
+            StringRequest strReq = new StringRequest(Request.Method.POST,
+                    //"http://localhost/interrupt/18/register.php",
+                    "http://192.168.13.217/interrupt/app_connect/login.php",
+                    // "http://192.168.0.4/interrupt/app_connect/register.php",
+                    //"http://localhost/interrupt/app_connect/register.php",
+                    //"https://keontaesemisi.000webhostapp.com/reg.php",
+                    //"http://172.31.99.160/interrupt/db/register.php",
+                    //"http://localhost/interrupt/app_db/register.php",
+                    new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d(TAG, "Login Response: " + response.toString());
+                            hideDialog();
+
+                            try {
+                                JSONObject jObj = new JSONObject(response);
+                                boolean error = jObj.getBoolean("error");
+
+                                if (!error) {
+
+                                    JSONObject user = jObj.getJSONObject("user");
+                                    String mobile = user.getString("mobileNo");
+                                    String password = user.getString("password");
+
+                                    Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+
+                                } else {
+
+
+                                    String errorMsg = jObj.getString("error_msg");
+                                    Toast.makeText(getApplicationContext(),
+                                            errorMsg, Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "Registration Error: " + error.getMessage());
+                    Toast.makeText(getApplicationContext(),
+                            error.getMessage(), Toast.LENGTH_LONG).show();
+                    hideDialog();
+                }
+            }) {
+
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("mobileNo", uid_mobile);
+                    params.put("password", login_pswd);
+                    return params;
+                }
+
+            };
+
+            AppController.getInstance().addToRequestQueue(strReq);
+        }
+
+                }
+
+    private void showDialog() {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+
+    private void hideDialog() {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
     }
 }
